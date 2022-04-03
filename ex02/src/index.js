55a73d55c6499bc678680d27bb83bb97390b277f
Stroil <edinstroil@hotmail.com>
2022-04-04 01:28:43 +0200

    Ispravka Repo03

---
+++ ex02/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // Only change code below this line
+    /* Vozdra raja mogu li vas ponudit olovkama
+    od kojih ja zivim */
+    // One liner comment
+    // Only change code above this line
+  }
+  console.log(main());
+  module.exports = main;
